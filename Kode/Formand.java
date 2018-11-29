67aa18702142c2e9d0df62ff7d8e8b7ae9350990
Jesrty <ravn1234567890@hotmail.com>
2018-11-29 09:22:53 +0100

    formand opdate

--- Kode/Formand.java
+++ Kode/Formand.java
@@ -61,6 +61,7 @@ public class Formand extends Employee{
    }
    
    public void redigerMedlem() throws FileNotFoundException{
+      
       Scanner s2 = new Scanner(file2);
       System.out.println("Type the full name you want to edit");
       String name = s.nextLine();
@@ -80,23 +81,87 @@ public class Formand extends Employee{
       }
       ss2.close();
       
-      
-      /*
-      for(int h = 0; h<medList.length; h++){
-      String[] medList22 = medList[h].split(",");
-      if(medList22[1].equals(name)){
-         h = medList.length;
-         System.out.println(h);
+      //Den splitter linjerne op og tester om navnet passer
+      //så splitter den den rigtig linje op igen 
+      //så vælger du hvad du vil ændre og ændre det og så skriver den det ind i filen igen
+      int f = 0;
+      for(int q = 0; q<medList.length; q++){
+         String firstSpllit[] = medList[q].split(",");
+         f++;  
+         if(firstSpllit[0].equals(name)){
+            q = medList.length;  
+         }
       }
-      }
       
-      String test222 = ss2.nextLine();
-      System.out.println(test222);
-      */
+      System.out.println(medList[(f-1)]);
+      String secondSpllit[] = medList[(f-1)].split(",");
+      
+      System.out.println("Type 1 for name, type 2 for birthday, type 3 for mail, type 4 for phone, type 5 for activ passive");
+      int nummer = s.nextInt();
+      System.out.println("Type change");
+      String gemmer = s.nextLine();
+      gemmer = s.nextLine();
+      secondSpllit[(nummer-1)] = gemmer;
+      medList[(f-1)] = secondSpllit[0] + "," + secondSpllit[1] + "," + secondSpllit[2] + "," + secondSpllit[3] + "," + secondSpllit[4];
+      
+      PrintStream outPrint11 = new PrintStream(file2); 
+      for(int l = 0; l<medList.length; l++){
+         outPrint11.println(medList[l]);
+      }
    
    }
    
+ public void redigerKonku() throws FileNotFoundException{
+      
+      Scanner s1 = new Scanner(file1);
+      System.out.println("Type the full name you want to edit");
+      String name = s.nextLine();
+      int i = 0;
+      
+      while(s1.hasNextLine()){
+         i++;
+         String placeholder = s1.nextLine();
+      }
+      String[] medList = new String[i];
+      s1.close();
+      
+      Scanner ss1 = new Scanner(file1);
+      
+      for(int p = 0; p<medList.length; p++){
+         medList[p] = ss1.nextLine();
+      }
+      ss1.close();
+      
+      //Den splitter linjerne op og tester om navnet passer
+      //så splitter den den rigtig linje op igen 
+      //så vælger du hvad du vil ændre og ændre det og så skriver den det ind i filen igen
+      int f = 0;
+      for(int q = 0; q<medList.length; q++){
+         String firstSpllit[] = medList[q].split(",");
+         f++;  
+         if(firstSpllit[0].equals(name)){
+            q = medList.length;  
+         }
+      }
+      
+      System.out.println(medList[(f-1)]);
+      String secondSpllit[] = medList[(f-1)].split(",");
+      
+      System.out.println("Type 1 for name, type 2 for birthday, type 3 for mail, type 4 for phone");
+      int nummer = s.nextInt();
+      System.out.println("Type change");
+      String gemmer = s.nextLine();
+      gemmer = s.nextLine();
+      secondSpllit[(nummer-1)] = gemmer;
+      medList[(f-1)] = secondSpllit[0] + "," + secondSpllit[1] + "," + secondSpllit[2] + "," + secondSpllit[3];
+      
+      PrintStream outPrint11 = new PrintStream(file1); 
+      for(int l = 0; l<medList.length; l++){
+         outPrint11.println(medList[l]);
+      }
    
+   }
+
    
 
 }
